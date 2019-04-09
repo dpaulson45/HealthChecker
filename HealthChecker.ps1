a85ba274629f60c70b509eac5df1fccac297ac6a
Lukas Sassl <lusassl@microsoft.com>
2019-04-09 19:54:55 +0200

    New vulnerabilities added
    + CVE-2019-0817
    + CVE-2019-0858

--- HealthChecker.ps1
+++ HealthChecker.ps1
@@ -2991,6 +2991,7 @@ param(
         #CVE-2018-0924 affects E2010 but we cannot check for them
 	#CVE-2019-0686 affects E2010 but we cannot check for them
 	#CVE-2019-0724 affects E2010 but we cannot check for them
+	#CVE-2019-0817 affects E2010 but we cannot check for them
         #could do get the build number of exsetup, but not really needed with Exchange 2010 as it is going out of support soon. 
         Write-Yellow("`nWe cannot check for more vulnerabilities for Exchange 2010.")
         Write-Yellow("You should make sure that your Exchange 2010 Servers are up to date with all security patches.")
@@ -3056,7 +3057,10 @@ param(
 	{
 	    if($exchangeCU -eq [HealthChecker.ExchangeCULevel]::CU22)
 	    {
-	        Write-Verbose("`nThere are no known vulnerabilities within Exchange 2013 CU22.")
+	        #CVE-2019-0817
+	        Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1473.4 -CVEName "CVE-2019-0817"
+	        #CVE-2019-0858
+	        Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1473.4 -CVEName "CVE-2019-0858"
 	    }
 	    #CVE-2019-0686
 	    Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1473.3 -CVEName "CVE-2019-0686"
@@ -3149,12 +3153,19 @@ param(
                 #CVE-2019-0588
                 Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1591.13 -CVEName "CVE-2019-0588"
             }
+	    #CVE-2019-0817
+	    Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1591.16 -CVEName "CVE-2019-0817"
+	    #CVE-2018-0858
+	    Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1591.16 -CVEName "CVE-2019-0858"
         }
 	if($exchangeCU -le [HealthChecker.ExchangeCULevel]::CU12)
 	{
 	    if($exchangeCU -eq [HealthChecker.ExchangeCULevel]::CU12)
 	    {
-	        Write-Verbose("`nThere are no current known vulnerabilities within Exchange 2016 CU12.")
+	        #CVE-2019-0817
+		Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1713.6 -CVEName "CVE-2019-0817"
+		#CVE-2018-0858
+		Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1713.6 -CVEName "CVE-2019-0858"
 	    }
 	    #CVE-2019-0686
 	    Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 1713.5 -CVEName "CVE-2019-0686"
@@ -3170,12 +3181,19 @@ param(
             Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 221.14 -CVEName "CVE-2019-0586"
             #CVE-2019-0588
             Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 221.14 -CVEName "CVE-2019-0588"
+	    #CVE-2019-0817
+	    Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 221.16 -CVEName "CVE-2019-0817"
+	    #CVE-2018-0858
+	    Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 221.16 -CVEName "CVE-2019-0858"
         }
 	if($exchangeCU -le [HealthChecker.ExchangeCULevel]::CU1)
 	{
 	    if($exchangeCU -eq [HealthChecker.ExchangeCULevel]::CU1)
 	    {
-	        Write-Verbose("`nThere are no current known vulnerabilities within Exchange 2019 CU1.")
+	        #CVE-2019-0817
+		Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 330.7 -CVEName "CVE-2019-0817"
+		#CVE-2018-0858
+		Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 330.7 -CVEName "CVE-2019-0858"
 	    }
 	    #CVE-2019-0686
 	    Test-VulnerabilitiesByBuildNumbersAndDisplay -ExchangeBuildRevision $buildRevision -SecurityFixedBuild 330.6 -CVEName "CVE-2019-0686"
