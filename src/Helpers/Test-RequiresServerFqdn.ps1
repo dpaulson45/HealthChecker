2c52f2a981fa3ad2b1d199c483119de0b58cd6e5
Lukas Sassl <lusassl@microsoft.com>
2021-01-22 11:01:18 +0100

    Test-RequiresServerFqdn improvements

--- src/Helpers/Test-RequiresServerFqdn.ps1
+++ src/Helpers/Test-RequiresServerFqdn.ps1
@@ -1,12 +1,21 @@
 Function Test-RequiresServerFqdn {
 
     Write-VerboseOutput("Calling: Test-RequiresServerFqdn")
-
-    try {
+    try
+    {
         $Script:ServerFQDN = (Get-ExchangeServer $Script:Server).FQDN
-        Invoke-Command -ComputerName $Script:Server -ScriptBlock { Get-Date | Out-Null } -ErrorAction Stop
-        Write-VerboseOutput("Connected successfully using NetBIOS name.")
-    } catch {
+        if($Script:Server -ne $env:COMPUTERNAME)
+        {
+            Invoke-Command -ComputerName $Script:Server -ScriptBlock {Get-Date | Out-Null} -ErrorAction Stop
+            Write-VerboseOutput("Connected successfully using NetBIOS name.")
+        }
+        else
+        {
+            Write-VerboseOutput("Executed against the local machine. No need to pass '-ComputerName' parameter.")
+        }
+    }
+    catch
+    {
         Invoke-CatchActions
         Write-VerboseOutput("Failed to connect to {0} using NetBIOS name. Fallback to Fqdn: {1}" -f $Script:Server, $Script:ServerFQDN)
         $Script:Server = $Script:ServerFQDN
